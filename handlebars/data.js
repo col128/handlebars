37dc4c89a9df92886cacd297f278cf3fa38c1f7c
col128 <col128@126.com>
2016-02-07 00:57:42 +0800

    Revert "Revert "basic handlebars""
    This reverts commit 3884a003850ccddcb628545a3b4a032e87b3dcde.

---
+++ handlebars/data.js
@@ -0,0 +1,22 @@
+/**
+ * Created by TRwin7SP1 on 2016/2/6.
+ */
+var mdata = {
+  student: [
+    {
+      "name": "张三",
+      "sex": "0",
+      "age": 18
+    },
+    {
+      "name": "李四",
+      "sex": "0",
+      "age": 22
+    },
+    {
+      "name": "妞妞",
+      "sex": "1",
+      "age": 18
+    }
+  ]
+}
