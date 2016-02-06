d6a19a4995a9bf27ddf9c363651ef842f6a0c897
col128 <col128@126.com>
2016-02-07 00:06:34 +0800

    循环中使用this

---
+++ handlebars/02/data.js
@@ -0,0 +1,44 @@
+/**
+ * Created by TRwin7SP1 on 2016/2/6.
+ */
+var data = [
+  {
+    "name": "张三",
+    "sex": "0",
+    "age": 18,
+    "favorite":
+      [
+        {
+          "name":"唱歌"
+        },{
+        "name":"篮球"
+      }
+      ]
+  },
+  {
+    "name": "李四",
+    "sex": "0",
+    "age": 22,
+    "favorite":
+      [
+        {
+          "name":"上网"
+        },{
+        "name":"足球"
+      }
+      ]
+  },
+  {
+    "name": "妞妞",
+    "sex": "1",
+    "age": 18,
+    "favorite":
+      [
+        {
+          "name":"电影"
+        },{
+        "name":"旅游"
+      }
+      ]
+  }
+];
