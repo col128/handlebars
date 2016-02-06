06ab69b265704ce2f1efa184779df734f8b8703e
col128 <col128@126.com>
2016-02-07 02:19:36 +0800

    自定义help01

---
+++ handlebars/05/data.js
@@ -0,0 +1,21 @@
+/**
+ * Created by TRwin7SP1 on 2016/2/6.
+ */
+var data = {
+  "student": [
+    {
+      "name": "张三",
+      "sex": "0",
+      "age": 18
+    },
+    {
+      "sex": "0",
+      "age": 22
+    },
+    {
+      "name": "妞妞",
+      "sex": "1",
+      "age": 18
+    }
+  ]
+};
---
+++ handlebars/05/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by TRwin7SP1 on 2016/2/6.
+ */
+$(function () {
+  var source= $("#student_item_template").html();
+
+
+
+
+  Handlebars.registerHelper("compare", function (v1,v2,options) {
+    if (v1 > v2)
+    {
+          return options.fn(this);
+    }
+    else
+    {
+      return options.inverse(this);
+    }
+  })
+  var template= Handlebars.compile(source);
+  var result = template(data);
+  $("#student-table-body").html(result);
+})
